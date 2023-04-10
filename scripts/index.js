08d5431439daf19a48a4f1d4f5d665a059badef1
kaylieryan <kayliemryan@gmail.com>
2023-04-10 11:23:51 -0700

    js update for modal box

--- scripts/index.js
+++ scripts/index.js
@@ -25,11 +25,63 @@ const initialCards = [
   },
 ];
 
+/*---------------------------------------------------------------------------------------------------------*/
+/*                                                Element                                                  */
+/*---------------------------------------------------------------------------------------------------------*/
+
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileEditCloseModal = document.querySelector("#profile-edit-modal");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
 
+/*---------------------------------------------------------------------------------------------------------*/
+/*                                                Functions                                                */
+/*---------------------------------------------------------------------------------------------------------*/
 
+function closePopup() {
+  profileEditCloseModal.classList.remove("modal__opened");
+}
 
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+
+  return cardElement;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*                                                Event Handlers                                           */
+/*---------------------------------------------------------------------------------------------------------*/
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*                                                Event Listeners                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+
+  profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal__opened");
 });
+
+profileEditCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
